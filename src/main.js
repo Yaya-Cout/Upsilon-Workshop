1e81598b403510dc616489b813da09e0a8aa6874
Yaya-Cout <yaya.cout@free.fr>
2022-09-09 17:14:44 +0200

    configure Vue.!js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import i18n from './i18n'
 
-createApp(App).mount('#app')
+createApp(App).use(i18n).use(i18n).use(router).mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  pluginOptions: {
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableLegacy: false,
+      runtimeOnly: false,
+      compositionOnly: false,
+      fullInstall: true
+    }
+  }
 })
