f7e4ca50c18773e207f2e093691359ef4f939367
Yaya-Cout <yaya.cout@free.fr>
2022-09-09 18:25:20 +0200

    Add VueX

--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import router from './router'
 import i18n from './i18n'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
+import store from './store'
 
 loadFonts()
 
 createApp(App)
+  .use(store)
   .use(router)
   .use(vuetify)
   .use(i18n)
