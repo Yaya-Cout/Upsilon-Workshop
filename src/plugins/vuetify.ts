ed80d29bcf54d6b9f657e2a01f40806d119a5707
Yaya-Cout <yaya.cout@free.fr>
2023-04-12 18:53:41 +0200

    [Vuetify] Use Material 3 blueprint

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,10 +1,14 @@
 // Styles
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
+import { md3 } from 'vuetify/blueprints'
 
 // Vuetify
 import { createVuetify } from 'vuetify'
 
 export default createVuetify(
   // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+  {
+    blueprint: md3
+  }
 )
