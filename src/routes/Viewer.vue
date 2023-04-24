83892a4b26f16ca4121e7333c0e6c9765d32cea7
Yaya-Cout <yaya.cout@free.fr>
2023-04-24 19:47:00 +0200

    [Viewer] Allow script uploading to calculator

--- src/routes/Viewer.vue
+++ src/routes/Viewer.vue
@@ -4,6 +4,7 @@
             <v-col>
                 <h1>{{ project?.title }}</h1>
                 <v-btn :to="'/edit/' + project?.uuid">Edit</v-btn>
+                <v-btn @click="upload()">Upload to calculator</v-btn>
                 <Markdown :content="project?.description" />
             </v-col>
             <v-col>
@@ -18,7 +19,8 @@ import { defineComponent } from 'vue';
 import Markdown from '../components/Markdown.vue';
 import Simulator from '../components/Simulator.vue';
 import { useAPIStore } from '../stores/api';
-import { Project } from '../types';
+import { useCalculatorStore } from '../stores/calculator';
+import { Project, Script } from '../types';
 
 export default defineComponent({
     name: 'Viewer',
@@ -27,10 +29,56 @@ export default defineComponent({
         return {
             project: null as Project | null,
             api: useAPIStore().api,
+            calculatorStore: useCalculatorStore(),
+            calculator: useCalculatorStore().calculator,
         };
     },
     async mounted() {
         this.project = await this.api.getProject(this.$route.params.uuid);
+    },
+    methods: {
+        async upload() {
+            if (this.project && this.connected) {
+                await this.uploadProject(this.project);
+            }
+        },
+        async uploadProject(project: Project) {
+            // TODO: Male a component for this.
+            let storage = await this.calculator.backupStorage();
+
+            for (let file of project.files) {
+                let name = file.title.split('.').shift();
+                let type = file.title.split('.').pop();
+                storage.records.push({
+                    name: name,
+                    type: type,
+                    // TODO: Store it on the API and retrieve it here.
+                    // (Or mark the file which have the name of the project to
+                    // be autoImported)
+                    autoImport: true,
+                    code: file.content,
+                });
+            }
+
+            // TODO: Handle errors like too much files.
+            await this.calculator.installStorage(storage, this.storageInstalled, this.storageInstallError);
+        },
+        storageInstalled() {
+            console.log("Storage installed");
+        },
+        storageInstallError(error: any) {
+            console.error("Storage install error: " + error);
+        },
+    },
+    computed: {
+        connected: {
+            get() {
+                return this.calculatorStore.connected;
+            },
+            set(value: boolean) {
+                this.calculatorStore.connected = value;
+            },
+        },
     },
 });
 </script>
