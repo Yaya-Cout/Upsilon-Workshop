9805fbf74d8b1f7e780743991674a60d2850b8d3
Yaya-Cout <yaya.cout@free.fr>
2023-04-25 10:09:51 +0200

    [ProjectPreview] Fix layout when multiple preview on row

--- src/components/ProjectPreview.vue
+++ src/components/ProjectPreview.vue
@@ -1,7 +1,10 @@
 <template>
-    <v-card class="ma-2 rounded-lg" elevation="4" :to="'/view/' + project.uuid">
-        <div class="flex justify-between">
-            <!-- <v-img
+    <!-- We use a div to avoid the v-card being a direct child of the v-row and
+        being affected by other v-cards in the same row -->
+    <div>
+        <v-card class="ma-2 rounded-lg" elevation="4" :to="'/view/' + project.uuid">
+            <div class="flex justify-between">
+                <!-- <v-img
                 class="rounded-lg"
                 elevation="4"
                 :aspect-ratio="320 / 240"
@@ -9,24 +12,31 @@
                 src="/assets/screenshot.png"
                 style="z-index: -1"
             /> -->
-            <div class="elevation-10">
-                <v-card-title class="pb-0">{{ project.title }}</v-card-title>
-                <v-card-text class="py-0">{{ project.author }}</v-card-text>
-                <v-card-subtitle>{{ project.description }}</v-card-subtitle>
-                <v-card-actions>
-                    <v-rating length="5" v-model="project.rating"></v-rating>
-                </v-card-actions>
+                <div class="elevation-10">
+                    <v-card-title class="pb-0">{{ project.title }}</v-card-title>
+                    <v-card-text class="py-0">{{ project.author }}</v-card-text>
+                    <v-card-subtitle>{{ project.description }}</v-card-subtitle>
+                    <v-card-actions>
+                        <v-rating length="5" v-model="project.rating"></v-rating>
+                    </v-card-actions>
+                </div>
             </div>
-        </div>
-    </v-card>
+        </v-card>
+    </div>
 </template>
+
 <script lang="ts">
 import { defineComponent, PropType } from 'vue';
 import { Project } from '../types';
 
 export default defineComponent({
     name: 'ProjectPreview',
-    props: { project: { type: Object as PropType<Project>, required: true } },
+    props: {
+        project: {
+            type: Object as PropType<Project>,
+            required: true
+        }
+    },
 });
 </script>
 
