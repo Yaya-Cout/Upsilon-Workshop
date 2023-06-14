19854e73ad293c73a1b1d68a471ffde8269bef82
Yaya-Cout <yaya.cout@free.fr>
2023-06-14 13:44:18 +0200

    [Simulator] Use dev simulator

--- download-epsilon-js.sh
+++ download-epsilon-js.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 # Download epsilon.js from Firebase Storage
-url="https://firebasestorage.googleapis.com/v0/b/upsilon-binfiles.appspot.com/o/official%2Fsimulator%2Fepsilon.python.min.js"
+url="https://firebasestorage.googleapis.com/v0/b/upsilon-binfiles.appspot.com/o/dev%2Fsimulator%2Fepsilon.js"
 token=$(curl "${url}" | grep -oP '(?<="downloadTokens": ")[^"]+')
 curl -L -o public/simulator/epsilon.js "${url}?alt=media&token=${token}"
