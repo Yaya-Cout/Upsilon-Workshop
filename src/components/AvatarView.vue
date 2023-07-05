e869b2f6f01a166dbd53ef34c73e2335c29e9010
Yaya-Cout <yaya.cout@free.fr>
2023-07-05 14:27:38 +0200

    [AvatarView] Rewrite using the composition API

--- src/components/AvatarView.vue
+++ src/components/AvatarView.vue
@@ -4,27 +4,23 @@
   </VAvatar>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
+<script setup lang="ts">
+import { computed } from "vue";
 import { toSvg } from "jdenticon";
 
-export default defineComponent({
-  name: 'AvatarView',
-  props: {
-    username: {
-      type: String,
-      required: true,
-    },
+const props = defineProps({
+  username: {
+    type: String,
+    required: true,
   },
-  computed: {
-    svg(): string {
-      let svg = toSvg(this.username, 100);
-      // Ensure that the SVG is not larger than the avatar
-      svg = svg.replace('width="100"', 'width="100%"');
-      svg = svg.replace('height="100"', 'height="100%"');
-      return svg;
-    },
-  },
+});
+
+const svg = computed(() => {
+  let svg = toSvg(props.username, 100);
+  // Ensure that the SVG is not larger than the avatar
+  svg = svg.replace('width="100"', 'width="100%"');
+  svg = svg.replace('height="100"', 'height="100%"');
+  return svg;
 });
 </script>
 
