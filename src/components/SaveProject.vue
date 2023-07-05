614d1b64e20959b8e9f2be653502542d09e41908
Yaya-Cout <yaya.cout@free.fr>
2023-07-05 17:53:56 +0200

    [SaveProject] Rewrite using the composition API

--- src/components/SaveProject.vue
+++ src/components/SaveProject.vue
@@ -4,37 +4,29 @@
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent, PropType } from 'vue';
+<script setup lang="ts">
 import { Project } from '../types';
 import { useAPIStore } from '../stores/api';
 import { useGlobalStore } from '../stores/global';
 
-export default defineComponent({
-  props: {
-    project: {
-      type: Object as PropType<Project>,
-      required: true,
-    },
-  },
-  data() {
-    return {
-      api: useAPIStore().api,
-      globalStore: useGlobalStore(),
-    };
-  },
-  methods: {
-    save() {
-      this.api.updateProject(this.project).then(() => {
-        this.globalStore.success = "snackbar.success.project-saved.message"
-      }).catch((error) => {
-        this.globalStore.error = true;
-        console.error(error);
-      });
-    },
-  },
+const api = useAPIStore().api;
+const globalStore = useGlobalStore();
 
+const props = defineProps({
+  project: {
+    type: Object as () => Project,
+    required: true,
+  },
 });
+
+const save = () => {
+  api.updateProject(props.project).then(() => {
+    globalStore.success = "snackbar.success.project-saved.message"
+  }).catch((error) => {
+    globalStore.error = true;
+    console.error(error);
+  });
+}
 </script>
 
 <style scoped></style>
