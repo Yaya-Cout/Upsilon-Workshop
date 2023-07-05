4eaec55e11ea415b2707b293b73c0fedb7dc17ab
Yaya-Cout <yaya.cout@free.fr>
2023-07-05 17:28:29 +0200

    [ProgressbarComponent] Rewrite using the composition API

--- src/components/ProgressbarComponent.vue
+++ src/components/ProgressbarComponent.vue
@@ -9,31 +9,22 @@
   />
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
+<script setup lang="ts">
+import { computed } from 'vue';
 import { useGlobalStore } from '../stores/global';
 
-export default defineComponent({
-    data() {
-        return {
-            globalStore: useGlobalStore(),
-        };
-    },
-    computed: {
-        progress() : number {
-            if (this.globalStore.progress === true || this.globalStore.progress === false) {
-                return 0;
-            }
-            return this.globalStore.progress;
-        },
-        indeterminate() : boolean {
-            return this.globalStore.progress === true;
-        },
-        active() : boolean {
-            return this.globalStore.progress !== false;
-        },
-    },
+const globalStore = useGlobalStore();
+
+const progress = computed(() => {
+  if (globalStore.progress === true || globalStore.progress === false) {
+    return 0;
+  }
+  return globalStore.progress;
 });
+
+const indeterminate = computed(() => globalStore.progress === true);
+
+const active = computed(() => globalStore.progress !== false);
 </script>
 
 <style scoped></style>
