51d685b06922cafbbe21781f7c38f19b7cd24627
Yaya-Cout <yaya.cout@free.fr>
2023-07-06 12:42:52 +0200

    [MainSnackbar] Rewrite using the composition API

--- src/components/snackbars/MainSnackbar.vue
+++ src/components/snackbars/MainSnackbar.vue
@@ -8,25 +8,13 @@
   <UsernameTakenSnackbar />
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
+<script setup lang="ts">
 import ConnectCalculatorSnackbar from './ConnectCalculatorSnackbar.vue';
 import GenericErrorSnackbar from './GenericErrorSnackbar.vue';
 import GenericSuccessSnackbar from './GenericSuccessSnackbar.vue';
 import NotLoggedInSnackbar from './NotLoggedInSnackbar.vue';
 import TooMuchDataErrorSnackbar from './TooMuchDataErrorSnackbar.vue';
 import UsernameTakenSnackbar from './UsernameTakenSnackbar.vue';
-
-export default defineComponent({
-  components: {
-    ConnectCalculatorSnackbar,
-    GenericErrorSnackbar,
-    GenericSuccessSnackbar,
-    NotLoggedInSnackbar,
-    TooMuchDataErrorSnackbar,
-    UsernameTakenSnackbar
-  }
-});
 </script>
 
 <style scoped></style>
