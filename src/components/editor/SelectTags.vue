1e8e01225968100bebcc532bef7e0a8a09c97a11
Yaya-Cout <yaya.cout@free.fr>
2023-07-06 11:38:21 +0200

    [SelectTags] Rewrite using the composition API

--- src/components/editor/SelectTags.vue
+++ src/components/editor/SelectTags.vue
@@ -11,63 +11,54 @@
   />
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
+<script setup lang="ts">
+import { ref, watch, computed } from 'vue';
 import { useAPIStore } from '../../stores/api';
 
-export default defineComponent({
-  name: "SelectTags",
-  props: {
-    modelValue: {
-      type: Array as () => string[],
-      required: true,
-    },
-  },
-  emits: ["update:modelValue"],
-  data() {
-    return {
-      tagsList: [] as string[],
-      loading: false,
-      search: "",
-      api: useAPIStore().api,
-    };
-  },
-  computed: {
-    tags: {
-      get(): string[] {
-        return this.modelValue
-      },
-      set(value: string[]) {
-        this.$emit("update:modelValue", value);
-      },
-    },
-  },
-  watch: {
-    search() {
-      this.searchTags();
-    },
-  },
-  mounted() {
-    this.searchTags();
-  },
-  methods: {
-    async searchTags() {
-      this.loading = true;
-      try {
-        let tags = await this.api.getTags(this.search);
-        let tagsList = [] as string[];
-        for (let tag of tags) {
-          tagsList.push(tag.name);
-        }
-        this.tagsList = tagsList;
-      } catch (e) {
-        console.error(e);
-      } finally {
-        this.loading = false;
-      }
-    },
+const props = defineProps({
+  modelValue: {
+    type: Array as () => string[],
+    required: true,
   },
 });
+
+const api = useAPIStore().api;
+
+const tagsList = ref([] as string[]);
+const loading = ref(false);
+const search = ref("");
+
+const emits = defineEmits(["update:modelValue"]);
+
+const tags = computed({
+  get() {
+    return props.modelValue;
+  },
+  set(value: string[]) {
+    emits("update:modelValue", value);
+  },
+});
+
+const searchTags = async () => {
+  loading.value = true;
+  try {
+    let tags = await api.getTags(search.value);
+    let NewTagsList = [] as string[];
+    for (let tag of tags) {
+      NewTagsList.push(tag.name);
+    }
+    tagsList.value = NewTagsList;
+
+  } catch (e) {
+    console.error(e);
+  } finally {
+    loading.value = false;
+  }
+};
+
+watch(search, () => {
+  searchTags();
+}, { immediate: true });
 </script>
 
 <style scoped></style>
