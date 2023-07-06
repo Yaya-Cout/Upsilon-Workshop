18417250763beab5907e80caf7ffcaf9c1c6dcb5
Yaya-Cout <yaya.cout@free.fr>
2023-07-06 10:33:47 +0200

    [UploadProject] Rewrite using the composition API

--- src/components/UploadProject.vue
+++ src/components/UploadProject.vue
@@ -4,89 +4,79 @@
   </span>
 </template>
 
-<script lang="ts">
-import { defineComponent, PropType } from 'vue';
-import { useCalculatorStore } from '../stores/calculator';
-import { useGlobalStore } from '../stores/global';
+<script setup lang="ts">
+import { computed } from 'vue';
 import { Project } from '../types';
+import { useGlobalStore } from '../stores/global';
+import { useCalculatorStore } from '../stores/calculator';
 
-export default defineComponent({
-    props: {
-        project: {
-            type: Object as PropType<Project>,
-            required: true
-        }
-    },
-    data() {
-        return {
-            calculatorStore: useCalculatorStore(),
-            calculator: useCalculatorStore().calculator,
-            globalStore: useGlobalStore(),
-        };
-    },
-    computed: {
-        connected: {
-            get() {
-                return this.calculatorStore.connected;
-            },
-            set(value: boolean) {
-                this.calculatorStore.connected = value;
-            },
-        },
-    },
-    methods: {
-        async upload() {
-            if (this.project) {
-                if (this.connected) {
-                    await this.uploadProject(this.project);
-                } else {
-                    this.calculatorStore.notConnectedError = true;
-                }
-            } else {
-                this.globalStore.error = true;
-                // TODO: Handle this error. (wait ?)
-                console.error("No project to upload");
-            }
-        },
-        async uploadProject(project: Project) {
-            let storage = await this.calculator.backupStorage();
+const globalStore = useGlobalStore();
+const calculatorStore = useCalculatorStore();
 
-            for (let file of project.files) {
-                let name = file.title.split('.').shift();
-                let type = file.title.split('.').pop();
-                // If the script already exists in the storage, we remove it.
-                storage.records = storage.records.filter((record) => record.name !== name);
-                // TODO: Show a popup to ask the user if he wants to overwrite the
-                // existing script.
-                storage.records.push({
-                    name: name,
-                    type: type,
-                    // TODO: Store it on the API and retrieve it here.
-                    // (Or mark the file which have the name of the project to
-                    // be autoImported)
-                    autoImport: true,
-                    code: file.content,
-                });
-            }
-
-            try {
-                await this.calculator.installStorage(storage, this.storageInstalled);
-            } catch (error) {
-                this.storageInstallError(error);
-            }
-        },
-        storageInstalled() {
-            this.globalStore.success = "snackbar.success.installation-success.message";
-        },
-        storageInstallError(error: any) {
-            if (error.message === "Too much data!") {
-                this.calculatorStore.tooMuchDataError = true;
-            } else {
-                this.globalStore.error = true;
-            }
-        },
-    },
+const props = defineProps({
+  project: {
+    type: Object as () => Project,
+    required: true,
+  },
 });
+
+const connected = computed(() => {
+  return calculatorStore.connected;
+});
+
+const upload = async () => {
+  if (props.project) {
+    if (connected.value) {
+      await uploadProject(props.project);
+    } else {
+      calculatorStore.notConnectedError = true;
+    }
+  } else {
+    globalStore.error = true;
+    // TODO: Handle this error. (wait ?)
+    console.error('No project to upload');
+  }
+};
+
+const uploadProject = async (project: Project) => {
+  let storage = await calculatorStore.calculator.backupStorage();
+
+  for (let file of project.files) {
+    let name = file.title.split('.').shift();
+    let type = file.title.split('.').pop();
+    // If the script already exists in the storage, we remove it.
+    storage.records = storage.records.filter((record) => record.name !== name);
+    // TODO: Show a popup to ask the user if he wants to overwrite the
+    // existing script.
+    storage.records.push({
+      name: name,
+      type: type,
+      // TODO: Store it on the API and retrieve it here.
+      // (Or mark the file which have the name of the project to
+      // be autoImported)
+      autoImport: true,
+      code: file.content,
+    });
+  }
+
+  try {
+    await calculatorStore.calculator.installStorage(storage, storageInstalled);
+  } catch (error) {
+    storageInstallError(error);
+  }
+};
+
+const storageInstalled = () => {
+  globalStore.success = 'snackbar.success.installation-success.message';
+};
+
+const storageInstallError = (error: any) => {
+  if (error.message === 'Too much data!') {
+    calculatorStore.tooMuchDataError = true;
+  } else {
+    globalStore.error = true;
+  }
+};
 </script>
 
 <style scoped></style>
