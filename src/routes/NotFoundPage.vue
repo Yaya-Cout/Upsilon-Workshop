f00744432d6da99017eefd2b4583fa802edabfee
Yaya-Cout <yaya.cout@free.fr>
2023-07-07 18:05:34 +0200

    [NotFoundPage] Rewrite using the composition API

--- src/routes/NotFoundPage.vue
+++ src/routes/NotFoundPage.vue
@@ -32,12 +32,7 @@
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-
-export default defineComponent({
-});
-</script>
+<script setup lang="ts"></script>
 
 <style scoped>
 #notfound-page {
