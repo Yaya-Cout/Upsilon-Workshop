6643ee66068de34922e2b1ac371445f9b60cb4e7
Yaya-Cout <yaya.cout@free.fr>
2023-07-07 17:55:26 +0200

    [HomePage] Rewrite using the composition API

--- src/routes/HomePage.vue
+++ src/routes/HomePage.vue
@@ -15,33 +15,28 @@
     </v-row>
   </div>
 </template>
-<script lang="ts">
 
-import { defineComponent } from 'vue';
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
 import { useAPIStore } from '../stores/api';
 import { useGlobalStore } from '../stores/global';
-import ProjectPreview from '../components/ProjectPreview.vue';
 import { Project } from '../types';
+import ProjectPreview from '../components/ProjectPreview.vue';
 
-export default defineComponent({
-  components: { ProjectPreview },
-  data() {
-    return {
-      projects: [] as Project[],
-      api: useAPIStore().api,
-      globalStore: useGlobalStore(),
-    }
-  },
-  async mounted() {
-    this.globalStore.progress = true;
-    try {
-      this.projects = await this.api.getProjects()
-    } catch (e) {
-      console.error("Failed to fetch projects:", e)
-      this.globalStore.error = "snackbar.error.home.fetch-projects-failed"
-    }
-    this.globalStore.progress = false;
-  },
+const projects = ref([] as Project[]);
+
+const api = useAPIStore().api;
+const globalStore = useGlobalStore();
+
+onMounted(async () => {
+  globalStore.progress = true;
+  try {
+    projects.value = await api.getProjects();
+  } catch (e) {
+    console.error("Failed to fetch projects:", e);
+    globalStore.error = "snackbar.error.home.fetch-projects-failed";
+  }
+  globalStore.progress = false;
 });
 </script>
 
