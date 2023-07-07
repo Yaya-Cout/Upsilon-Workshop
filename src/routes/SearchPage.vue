ef63d4717d0ad57a009155683275625b7ee7d35f
Yaya-Cout <yaya.cout@free.fr>
2023-07-07 18:16:34 +0200

    [SearchPage] Rewrite using the composition API

--- src/routes/SearchPage.vue
+++ src/routes/SearchPage.vue
@@ -54,117 +54,110 @@
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
+<script setup lang="ts">
+import { ref, onMounted, computed, watch } from 'vue';
 import { useAPIStore } from '../stores/api';
 import { useGlobalStore } from '../stores/global';
 import { Project, Tag } from '../types';
 import ProjectPreview from '../components/ProjectPreview.vue';
 
-export default defineComponent({
-  components: {
-    ProjectPreview,
-  },
-  data() {
-    return {
-      projects: [] as Project[],
-      api: useAPIStore().api,
-      globalStore: useGlobalStore(),
-      timeout: null as any,
-      debouncedQuery: '' as string,
-      empty: true,
-      tags: [] as Tag[],
-      activeTags: [] as Tag[],
-    }
-  },
-  computed: {
-    query: {
-      get() {
-        return this.debouncedQuery;
-      },
-      set(value: string) {
-        clearTimeout(this.timeout);
-        this.timeout = setTimeout(() => {
-          this.debouncedQuery = value;
-        }, 500);
-      }
-    }
-  },
-  watch: {
-    debouncedQuery: async function () {
-      this.loadMore(true);
-    }
-  },
-  async mounted() {
-    this.loadMore();
-    this.loadTags();
-  },
-  methods: {
-    async loadMore(clear: boolean = false) {
-      this.globalStore.progress = true;
-      let page = this.projects.length / this.api.ITEM_PER_PAGE + 1;
-      // If clear is true, we want to reset the page to 1
-      if (clear === true) {
-        page = 1;
-      }
-      // If page is not an integer, we have reached the end of the list
-      else if (!Number.isInteger(page)) {
-        this.globalStore.progress = false;
-        this.empty = true;
-        return;
-      }
-      // Else round it down
-      let newProjects: Project[];
-      try {
-        newProjects = await this.api.getProjects(this.debouncedQuery, page, this.activeTags);
-      } catch (e) {
-        this.globalStore.progress = false;
-        this.globalStore.error = true;
-        console.error(e);
-        return;
-      }
+const projects = ref([] as Project[]);
+// TODO: Find the correct type
+const timeout = ref(null as any);
+const debouncedQuery = ref('');
+const empty = ref(true);
+const tags = ref([] as Tag[]);
+const activeTags = ref([] as Tag[]);
 
-      if (clear === true) {
-        this.projects = [];
-      }
+const api = useAPIStore().api;
+const globalStore = useGlobalStore();
 
-      // If newProjects is empty, we have reached the end of the list
-      if (newProjects.length === 0) {
-        this.globalStore.progress = false;
-        this.empty = true;
-        return;
-      }
-
-      this.projects = this.projects.concat(newProjects);
-      // If there is less projects than the number of items per page, we have reached the end of the list
-      if (newProjects.length < this.api.ITEM_PER_PAGE) {
-        this.empty = true;
-      } else {
-        this.empty = false;
-      }
-      // TODO: Read if empty from API
-      this.globalStore.progress = false;
-    },
-    async loadTags() {
-      try {
-        this.tags = await this.api.getTags();
-      } catch (e) {
-        this.globalStore.progress = false;
-        this.globalStore.error = true;
-        console.error(e);
-        return;
-      }
-    },
-    toogleTag(tag: Tag) {
-      if (this.activeTags.includes(tag)) {
-        this.activeTags = this.activeTags.filter(t => t !== tag);
-      } else {
-        this.activeTags.push(tag);
-      }
-      this.loadMore(true);
-    }
+const query = computed({
+  get() {
+    return debouncedQuery.value;
+  },
+  set(value: string) {
+    clearTimeout(timeout.value);
+    timeout.value = setTimeout(() => {
+      debouncedQuery.value = value;
+    }, 500);
   }
 });
+
+watch(debouncedQuery, async function () {
+  await loadMore(true);
+});
+
+onMounted(async () => {
+  await loadMore();
+  await loadTags();
+});
+
+const loadMore = async (clear: boolean = false) => {
+  globalStore.progress = true;
+  let page = projects.value.length / api.ITEM_PER_PAGE + 1;
+  // If clear is true, we want to reset the page to 1
+  if (clear === true) {
+    page = 1;
+  }
+  // If page is not an integer, we have reached the end of the list
+  else if (!Number.isInteger(page)) {
+    globalStore.progress = false;
+    empty.value = true;
+    return;
+  }
+  // Else round it down
+  let newProjects: Project[];
+  try {
+    newProjects = await api.getProjects(debouncedQuery.value, page, activeTags.value);
+  } catch (e) {
+    globalStore.progress = false;
+    globalStore.error = true;
+    console.error(e);
+    return;
+  }
+
+  if (clear === true) {
+    projects.value = [];
+  }
+
+  // If newProjects is empty, we have reached the end of the list
+  if (newProjects.length === 0) {
+    globalStore.progress = false;
+    empty.value = true;
+    return;
+  }
+
+  projects.value = projects.value.concat(newProjects);
+  // If there is less projects than the number of items per page, we have reached the end of the list
+  if (newProjects.length < api.ITEM_PER_PAGE) {
+    empty.value = true;
+  } else {
+    empty.value = false;
+  }
+  // TODO: Read if empty from API
+  globalStore.progress = false;
+};
+
+const loadTags = async () => {
+  try {
+    tags.value = await api.getTags();
+  } catch (e) {
+    globalStore.progress = false;
+    globalStore.error = true;
+    console.error(e);
+    return;
+  }
+};
+
+const toogleTag = (tag: Tag) => {
+  if (activeTags.value.includes(tag)) {
+    activeTags.value = activeTags.value.filter(t => t !== tag);
+  } else {
+    activeTags.value.push(tag);
+  }
+  loadMore(true);
+};
 </script>
 
 <style scoped>
