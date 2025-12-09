8b95715a76bcd0f4bb6d167084c64aeb46a338be
Yaya-Cout <yaya.cout@ik.me>
2025-12-09 18:39:41 +0100

    [Simulator] Download simulator from Upsilon binfiles

--- download-epsilon-js.sh
+++ download-epsilon-js.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env sh
-# Download epsilon.js from Firebase Storage
-url="https://firebasestorage.googleapis.com/v0/b/upsilon-binfiles.appspot.com/o/dev%2Fsimulator%2Fepsilon.js"
-token=$(curl "${url}" | grep -oP '(?<="downloadTokens": ")[^"]+')
-curl -L -o public/simulator/epsilon.js "${url}?alt=media&token=${token}"
+# # Download epsilon.js from Firebase Storage
+# url="https://firebasestorage.googleapis.com/v0/b/upsilon-binfiles.appspot.com/o/dev%2Fsimulator%2Fepsilon.js"
+# token=$(curl "${url}" | grep -oP '(?<="downloadTokens": ")[^"]+')
+# curl -L -o public/simulator/epsilon.js "${url}?alt=media&token=${token}"
+
+# Download epsilon.js from Upsilon binfiles
+url="https://raw.githubusercontent.com/Yaya-Cout/Upsilon-binfiles/refs/heads/main/binaries/dev/simulator/epsilon.js"
+curl -L -o public/simulator/epsilon.js "${url}"
 
 # Downoad epsilon-casworks.js from Parisse's website
 url="https://www-fourier.univ-grenoble-alpes.fr/~parisse/numworks/epsilon.js"
