6d060492313d1d16e32e4ae760a0d5a5f2f76985
Yaya-Cout <yaya.cout@ik.me>
2026-01-30 14:57:24 +0100

    [Download Epsilon] Retry CASWorks download

--- download-epsilon-js.sh
+++ download-epsilon-js.sh
@@ -1,8 +1,12 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+
+# Crash on error, to prevent a partial deployment
+set -e
 # # Download epsilon.js from Firebase Storage
 # url="https://firebasestorage.googleapis.com/v0/b/upsilon-binfiles.appspot.com/o/dev%2Fsimulator%2Fepsilon.js"
 # token=$(curl "${url}" | grep -oP '(?<="downloadTokens": ")[^"]+')
 # curl -L -o public/simulator/epsilon.js "${url}?alt=media&token=${token}"
+
 
 # Download epsilon.js from Upsilon binfiles
 url="https://raw.githubusercontent.com/Yaya-Cout/Upsilon-binfiles/refs/heads/main/binaries/dev/simulator/epsilon.js"
@@ -10,19 +14,19 @@ curl -L -o public/simulator/epsilon.js "${url}"
 
 if [ $CASWORKS_FALLBACK_TO_CURRENT_VERSION ]
 then
-# Downoad epsilon-casworks.js from current Upsilon Workshop deployment
-url="https://yaya-cout.github.io/Upsilon-Workshop/simulator/epsilon-casworks.js"
-curl -L -o public/simulator/epsilon-casworks.js "${url}"
+    # Downoad epsilon-casworks.js from current Upsilon Workshop deployment
+    url="https://yaya-cout.github.io/Upsilon-Workshop/simulator/epsilon-casworks.js"
+    curl -L -o public/simulator/epsilon-casworks.js "${url}"
 
-# Download xcas-ups.js from current Upsilon Workshop deployment
-url="https://yaya-cout.github.io/Upsilon-Workshop/simulator/xcas-ups.js"
-curl -L -o public/simulator/xcas-ups.js "${url}"
+    # Download xcas-ups.js from current Upsilon Workshop deployment
+    url="https://yaya-cout.github.io/Upsilon-Workshop/simulator/xcas-ups.js"
+    curl -L -o public/simulator/xcas-ups.js "${url}"
 else
-# Downoad epsilon-casworks.js from Parisse's website
-url="https://www-fourier.univ-grenoble-alpes.fr/~parisse/numworks/epsilon.js"
-curl -L -o public/simulator/epsilon-casworks.js "${url}"
+    # Downoad epsilon-casworks.js from Parisse's website
+    url="https://www-fourier.univ-grenoble-alpes.fr/~parisse/numworks/epsilon.js"
+    curl --retry 12 -L -o public/simulator/epsilon-casworks.js "${url}"
 
-# Download xcas-ups.js from Parisse's website
-url="https://www-fourier.univ-grenoble-alpes.fr/~parisse/numworks/xcas_ups.js"
-curl -L -o public/simulator/xcas-ups.js "${url}"
+    # Download xcas-ups.js from Parisse's website
+    url="https://www-fourier.univ-grenoble-alpes.fr/~parisse/numworks/xcas_ups.js"
+    curl --retry 12 -L -o public/simulator/xcas-ups.js "${url}"
 fi
